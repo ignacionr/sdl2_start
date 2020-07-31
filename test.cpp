a1dec54e9571359ef41ab6cf1f9658919e3dca59
Praveen Liyanaarachchi <hello@praveen.one>
2020-08-01 07:38:40 +0900

    SDL Surface and SDL color make in use

--- test.cpp
+++ test.cpp
@@ -4,10 +4,9 @@
 #include <iostream>
 #include <list>
 
-#include "sdlsurface.h"
+#include "sdladapters.h"
 #include "scene.h"
 #include "scenemanager.h"
-#include "sdltexture.h"
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -56,46 +55,40 @@ int Program::run(int speed)
     }
     SDL_Rect rc{0, 0, 100, 100};
 
-    auto renderer{SDL_CreateRenderer(_window,
-                                     -1,
-                                     SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE)}; // render is an abstract class
-    if (!renderer)
-    {
-        std::cerr << "Could not create a renderer :" << SDL_GetError() << std::endl;
-        return -1;
-    }
+    auto renderer{std::make_unique<sdl::Renderer>(_window)};
 
-    std::unique_ptr<SDLTexture> cat_texture;
+    std::unique_ptr<sdl::Texture> cat_texture;
     {
-        SDLSurface cat("rsc/download.jpeg");
-        cat_texture = std::make_unique<SDLTexture>(renderer, cat);
+        sdl::Surface cat("rsc/download.jpeg");
+        cat_texture = std::make_unique<sdl::Texture>(*renderer, cat);
     } // the destructor for cat is called right here
 
     SDL_Event ev;
     int center = (SCREEN_HEIGHT / 2) - rc.h;
+    sdl::Color white{255, 255, 255};
+
     for (auto waitResult = SDL_PollEvent(&ev); !waitResult || ev.type != SDL_QUIT; waitResult = SDL_PollEvent(&ev))
     {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-        SDL_RenderClear(renderer);
+        renderer->SetDrawColor(white);
+        //    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        renderer->Clear();
+        //SDL_RenderClear(renderer);
 
-        SDL_RenderCopy(renderer, *cat_texture, NULL, &rc);
+        renderer->Copy(*cat_texture, nullptr, &rc);
+        //SDL_RenderCopy(renderer, *cat_texture, NULL, &rc);
 
         // HOMEWORK: the speed of movement is currently 4 (static)
         //           SCREEN_HEIGHT is the total of the movement, after which it will wrapup [0-SCREEN_HEIGHT)
         //           let's make it so the speed is greater the farther the image is from the center
         //           center = SCREEN_HEIGHT / 2
         //           distance_to_center = abs(rc.y - center)
-        if (rc.y > center)
-        {
-            rc.y += speed * 2;
-        }
-        else
-        {
-            rc.y += speed;
-        }
+        auto distance_to_center = abs(rc.y - center);
+        auto offset = distance_to_center * speed / center + 1;
+
+        rc.y += offset;
 
         rc.y %= SCREEN_HEIGHT;
-        SDL_RenderPresent(renderer);
+        renderer->Present();
     }
 
     SDL_Quit();
@@ -106,7 +99,7 @@ int main(int argc, char *args[])
 {
     // create an instance of the program
     Program program;
-    return program.run(4);
+    return program.run(10);
     // run it
     // return the value yielded
 }

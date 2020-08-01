39a2da866f6a9f67d74cbfefd23f7ec8d910f09a
Praveen Liyanaarachchi <hello@praveen.one>
2020-08-02 01:04:20 +0900

    Add 3 rects in same window

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <iostream>
 #include <list>
+#include <thread>
 
 #include "sdladapters.h"
 #include "scene.h"
@@ -16,6 +17,7 @@ class Program
 public:
     ~Program();
     int run(int speed);
+    int getOffset(int speed, int rectY, int center);
 
 private:
     SDL_Window *_window{nullptr};
@@ -53,7 +55,10 @@ int Program::run(int speed)
         std::cerr << "Could not create a window :" << SDL_GetError() << std::endl;
         return -1;
     }
-    SDL_Rect rc{0, 0, 100, 100};
+
+    SDL_Rect rc[3] = {{0, 0, 100, 100},
+                      {100, 0, 100, 100},
+                      {200, 0, 100, 100}};
 
     auto renderer{std::make_unique<sdl::Renderer>(_window)};
 
@@ -64,7 +69,7 @@ int Program::run(int speed)
     } // the destructor for cat is called right here
 
     SDL_Event ev;
-    int center = (SCREEN_HEIGHT / 2) - rc.h;
+    int center = (SCREEN_HEIGHT / 2) - rc[0].h;
     sdl::Color white{255, 255, 255};
 
     for (auto waitResult = SDL_PollEvent(&ev); !waitResult || ev.type != SDL_QUIT; waitResult = SDL_PollEvent(&ev))
@@ -74,25 +79,38 @@ int Program::run(int speed)
         renderer->Clear();
         //SDL_RenderClear(renderer);
 
-        renderer->Copy(*cat_texture, nullptr, &rc);
-        //SDL_RenderCopy(renderer, *cat_texture, NULL, &rc);
+        renderer->Copy(*cat_texture, nullptr, &rc[0]);
+        renderer->Copy(*cat_texture, nullptr, &rc[1]);
+        renderer->Copy(*cat_texture, nullptr, &rc[2]);
 
         // HOMEWORK: the speed of movement is currently 4 (static)
         //           SCREEN_HEIGHT is the total of the movement, after which it will wrapup [0-SCREEN_HEIGHT)
         //           let's make it so the speed is greater the farther the image is from the center
         //           center = SCREEN_HEIGHT / 2
         //           distance_to_center = abs(rc.y - center)
-        auto distance_to_center = abs(rc.y - center);
-        auto offset = distance_to_center * speed / center + 1;
+        // auto distance_to_center = abs(rc.y - center);
+        // auto offset = distance_to_center * speed / center + 1;
 
-        rc.y += offset;
+        rc[0].y += getOffset(speed, rc[0].y, center);
+        rc[0].y %= SCREEN_HEIGHT;
 
-        rc.y %= SCREEN_HEIGHT;
+        rc[1].y += getOffset(12, rc[1].y, center);
+        rc[1].y %= SCREEN_HEIGHT;
+
+        rc[2].y += getOffset(14, rc[2].y, center);
+        rc[2].y %= SCREEN_HEIGHT;
+
         renderer->Present();
     }
 
     SDL_Quit();
     return 0;
+}
+
+int Program::getOffset(int speed, int rectY, int center)
+{
+    auto distance_to_center = abs(rectY - center);
+    return distance_to_center * speed / center + 1;
 }
 
 int main(int argc, char *args[])
